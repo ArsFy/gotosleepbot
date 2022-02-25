ff7cca77e798635f8a55c4b7d88583bee2db99cb
arsfy <arsfy@outlook.com>
2022-02-25 23:39:33 +0800

    up

--- main.js
+++ main.js
@@ -14,7 +14,6 @@ bot.on('message', (msg) => {
         }
     }else{
         let msgInfo = msg.text.split(" ");
-        // console.log(msg)
         switch (msgInfo[0]){
             case "/start":
             case "/start"+config.bot:
@@ -22,7 +21,7 @@ bot.on('message', (msg) => {
                 break;
             case "/help":
             case "/help"+config.bot:
-                bot.sendMessage(msg.chat.id, "使用幫助!\n關於Bot: /start\n幫群友設定睡眠提醒：/addsleep @username hour:minute", {"reply_to_message_id": msg.message_id});
+                bot.sendMessage(msg.chat.id, "使用幫助!\n關於Bot: /start\n幫群友設定睡眠提醒：/addsleep @username hour:minute\n給自己設定睡眠提醒：/addforme hour:minute", {"reply_to_message_id": msg.message_id});
                 break;
             case "/addsleep":
             case "/addsleep"+config.bot:
@@ -52,9 +51,43 @@ bot.on('message', (msg) => {
                                 bot.sendMessage(chatid, `${username} 睡覺時間到了！`);
                             });
                         })(msgInfo[1], msg.chat.id)
-                        bot.sendMessage(msg.chat.id, `設定提醒：${tday} ${hour}:${minute}，我會${msg.chat.username!=undefined?"在 @"+msg.chat.username+" ":""}催 ${msgInfo[1]} 睡覺哦！`, {"reply_to_message_id": msg.message_id});
+                        bot.sendMessage(msg.chat.id, `設定提醒：${tday} ${hour<10?"0"+hour:hour}:${minute<10?"0"+minute:minute}，我會${msg.chat.username!=undefined?"在 @"+msg.chat.username+" ":""}提醒 ${msgInfo[1]} 睡覺哦！`, {"reply_to_message_id": msg.message_id});
                     }else{
                         bot.sendMessage(msg.chat.id, "格式錯誤：/addsleep @username hour:minute", {"reply_to_message_id": msg.message_id});
+                    }
+                }
+                break;
+            case "/addforme":
+            case "/addforme"+config.bot:
+                if (msgInfo[1] == undefined){
+                    bot.sendMessage(msg.chat.id, "缺少參數：/addforme hour:minute", {"reply_to_message_id": msg.message_id});
+                }else{
+                    let date = msgInfo[1].split(":");
+                    let hour = Number(date[0]);
+                    let minute = Number(date[1]);
+                    if (!isNaN(hour) && !isNaN(minute)){
+                        let nowTime = new Date();
+                        let setDate, tday;
+                        if (hour <= nowTime.getHours()){
+                            if (minute <= nowTime.getMinutes()){
+                                setDate = new Date(nowTime.getFullYear(), nowTime.getMonth(), new Date(nowTime.getTime()+24*60*60*1000).getDate(), Number(date[0]), Number(date[1]), 0);
+                                tday = "明天";
+                            }else{
+                                setDate = new Date(nowTime.getFullYear(), nowTime.getMonth(), nowTime.getDate(), Number(date[0]), Number(date[1]), 0);
+                                tday = "今天";
+                            }
+                        }else{
+                            setDate = new Date(nowTime.getFullYear(), nowTime.getMonth(), nowTime.getDate(), Number(date[0]), Number(date[1]), 0);
+                            tday = "今天";
+                        }
+                        (async (username, chatid)=>{
+                            schedule.scheduleJob(setDate, ()=>{
+                                bot.sendMessage(chatid, `@${username} 睡覺時間到了！`);
+                            });
+                        })(msg.from.username, msg.chat.id)
+                        bot.sendMessage(msg.chat.id, `設定提醒：${tday} ${hour<10?"0"+hour:hour}:${minute<10?"0"+minute:minute}，我會${msg.chat.username!=undefined?"在 @"+msg.chat.username+" ":""}提醒你睡覺哦！`, {"reply_to_message_id": msg.message_id});
+                    }else{
+                        bot.sendMessage(msg.chat.id, "格式錯誤：/addforme hour:minute", {"reply_to_message_id": msg.message_id});
                     }
                 }
                 break;
